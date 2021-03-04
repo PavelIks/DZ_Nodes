7b4ef5a463e85ce2afb60f0e57e7de9ea7f5f90c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-03-04 20:07:25 +0200

    Updated-3.

--- app/src/main/java/com/example/mynodes/ClassJava/NodeResourceParser.java
+++ app/src/main/java/com/example/mynodes/ClassJava/NodeResourceParser.java
@@ -1,64 +1,82 @@
 package com.example.mynodes.ClassJava;
-
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
-
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class NodeResourceParser {
+public class NodeResourceParser
+{
     private ArrayList<Node> nodes;
-    public NodeResourceParser(){
+    public NodeResourceParser()
+    {
         nodes = new ArrayList<>();
     }
 
-    public ArrayList<Node> getNodes() {
+    public ArrayList<Node> getNodes()
+    {
         return nodes;
     }
 
-    public boolean parse(XmlPullParser xpp){
+    public boolean parse(XmlPullParser xpp)
+    {
         boolean status = true;
         Node currentNode = null;
         boolean inEntity = false;
         String textValue = "";
 
-        try {
+        try
+        {
             int eventType = xpp.getEventType();
-
-            while (eventType != XmlPullParser.END_DOCUMENT){
+            while (eventType != XmlPullParser.END_DOCUMENT)
+            {
                 String tagName= xpp.getName();
-                switch (eventType){
-                    case XmlPullParser.START_TAG:{
-                        if("node".equalsIgnoreCase(tagName)){
-                            inEntity = true;
-                            currentNode = new Node();
-                        }
-                        break;
-                    }
-                    case XmlPullParser.TEXT:{
-                        textValue = xpp.getText();
-                        break;
-                    }
-                    case XmlPullParser.END_TAG:{
-                        if(inEntity) {
-                            if ("node".equalsIgnoreCase(tagName)) {
-                                inEntity = false;
-                                nodes.add(currentNode);
-                            } else if ("title".equalsIgnoreCase(tagName)) {
-                                currentNode.setTitle(textValue);
-                            } else if ("body".equalsIgnoreCase(tagName)) {
-                                currentNode.setBody(textValue);
+                switch (eventType)
+                {
+                    case XmlPullParser.START_TAG:
+                        {
+                            if("node".equalsIgnoreCase(tagName))
+                            {
+                                inEntity = true;
+                                currentNode = new Node();
                             }
+                            break;
                         }
-                        break;
-                    }
+                        case XmlPullParser.TEXT:
+                        {
+                            textValue = xpp.getText();
+                            break;
+                        }
+                        case XmlPullParser.END_TAG:
+                        {
+                            if(inEntity)
+                            {
+                                if ("node".equalsIgnoreCase(tagName))
+                                {
+                                    inEntity = false;
+                                    nodes.add(currentNode);
+                                }
+                                else if ("title".equalsIgnoreCase(tagName))
+                                {
+                                    currentNode.setTitle(textValue);
+                                }
+                                else if ("body".equalsIgnoreCase(tagName))
+                                {
+                                    currentNode.setBody(textValue);
+                                }
+                            }
+                            break;
+                        }
                 }
                 eventType = xpp.next();
             }
-        } catch (XmlPullParserException e) {
+        }
+        catch (XmlPullParserException e)
+        {
             status = false;
             e.printStackTrace();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
         return status;
